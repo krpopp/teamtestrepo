d8e88e9844dca59288a86969d4f0915005eb8e9d
Karina <karina.popp@nyu.edu>
2021-11-09 08:47:57 -0500

    removed movement

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,45 +4,6 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public float speed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //i have made some changes!
-        //wow!
-        //game of the year
-
-        //i see those changes
-        //amazing!
-        //so glad to be working with you, comrade
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        MoveChar();
-    }
-
-    void MoveChar()
-    {
-        Vector3 newPos = transform.position;
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            newPos.y += speed * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            newPos.y -= speed * Time.deltaTime;
-        }else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            newPos.x += speed * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            newPos.x -= speed * Time.deltaTime;
-        }
-        transform.position = newPos;
-    }
+    
 
 }
